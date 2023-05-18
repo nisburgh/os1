2e2ec6886fa79c3ce9fbbcfc08ac66b109b06d6e
Nathan Isburgh <nathan.isburgh@edgecloud.com>
2023-05-18 21:26:03 +0000

    Lab 05 answers

---
+++ lab05/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+)
+
+var pageView int64
+
+func main() {
+        http.HandleFunc("/", handler)
+        log.Printf("Server starting and listening on 0.0.0.0:8080...")
+        log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+        log.Printf("Ping from %s", r.RemoteAddr)
+        pageView++
+        fmt.Fprintf(w, "Hello, you're visitor #%d !", pageView)
+}
